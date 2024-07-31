77b8ef43a52a26a51ca246f0c9d44d08f342129d
Mushe <mosescolince161@gmail.com>
2024-07-31 11:19:18 +0200

    using fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 //import apiservice from './apiservice';
 import './App.css';
 import { useEffect,useState } from 'react';
+//import axios from 'axios';
 
 const api = {
   key: "9d23474ef78f20a41c316fde70fb3294",
-  base: "https://api.openweathermAP.ORG/DATA/2.5/"
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
 const [search, setSearch] = useState("");
+const [weather, setWeather] = useState({})
 
 const searchPressed = () => {
-  fetch(`${api.base}weather?q`)
-}
+  fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+ .then((res) => res.json())
+ .then((results)=>{
+  setWeather(results)
+ });
+};
   return (
     <div className="App">
     <header className='app-header'>
@@ -32,13 +38,14 @@ const searchPressed = () => {
        <button onClick={searchPressed}> Search</button>
       </div>
       {/*Location*/}
-      <p> Thembisa</p>
+      <p> {weather.name} *C</p>
 
       {/*Temperature*/}
-      <p> 33 *F</p>
+      <p>{weather.main.temp}</p>
 
       {/*Weather Condition*/}
-      <p>Sunny</p>
+      <p>{weather.weather[0].main}</p>
+      <p>({weather.weather[0].description})</p>
 
     </header>
     </div>
